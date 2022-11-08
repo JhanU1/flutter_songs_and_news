57cfb533ad5b0e407f422148283fa841fdfd64a2
Jhan Carlos Utria Z <jutria@juridigo.com>
2022-11-08 17:38:55 -0500

    feat: news model and news widget

---
+++ lib/new/model/new_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class New {
+  String title;
+  String content;
+  Color color;
+
+  New({required this.title, required this.content, required this.color});
+}
---
+++ lib/new/widgets/new_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../model/new_model.dart';
+
+class NewWidget extends StatelessWidget {
+  NewWidget({super.key, required this.newModel});
+
+  New newModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Card(
+        elevation: 1.5,
+        margin: const EdgeInsets.fromLTRB(6, 12, 6, 0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: InkWell(
+          // Make it splash on Android. It would happen automatically if this
+          // was a real card but this is just a demo. Skip the splash on iOS.
+          onTap: defaultTargetPlatform == TargetPlatform.iOS ? null : () {},
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: newModel.color,
+                  child: Text(
+                    newModel.title.substring(0, 1),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(left: 16)),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        newModel.title,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 8)),
+                      Text(
+                        newModel.content,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
